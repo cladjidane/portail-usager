43664709dd7b36aa4a913b4352eac782134ae475
Marc Gavanier <marc.gavanier@gmail.com>
2022-01-18 14:18:01 +0100

    fix beta api domain name in production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -4,8 +4,7 @@ import { Api, EnvironmentModel } from './environment.model';
 // TODO Passer en var d'env
 export const ENVIRONMENT: EnvironmentModel = {
   apisConfiguration: {
-    // Todo: remove before production (really need to set up environment variable to handle this case...)
-    [Api.ConseillerNumerique]: { domain: 'https://beta.api.conseiller-numerique.gouv.fr' }
+    [Api.ConseillerNumerique]: { domain: 'https://api.conseiller-numerique.gouv.fr' }
   },
   type: EnvironmentType.Production
 };
