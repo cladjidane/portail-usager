7d6531fe6fe0dc97965f1c5f6de5a2934ebf797b
Sébastien Quirrion <77339872+squirrion@users.noreply.github.com>
2022-01-27 15:11:04 +0100

    Ajout tracker matomo (#66)

---
+++ src/scripts/tracker.js
@@ -0,0 +1,14 @@
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.conseiller-numerique.gouv.fr"]);
+_paq.push(["setDomains", ["*.conseiller-numerique.gouv.fr"]]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://stats.data.gouv.fr/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '147']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
